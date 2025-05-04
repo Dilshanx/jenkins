e7c98ba2b5df661f6f5f00a1bbe8a8ee1431fc11
Dilshan Jayatissa <149313110+Dilshanx@users.noreply.github.com>
2025-05-04 18:19:47 +0530

    Update build.sh

--- Bash/build.sh
+++ Bash/build.sh
@@ -3,7 +3,7 @@ sudo docker rmi -f $(sudo docker images -q) ##this is not recommned step, i am d
 sudo rm -r gold ## these steps are not recommened instead you can modify script as shown below
 sudo mkdir gold
 cd gold/
-sudo git clone https://github.com/sagarkakkalasworld/Day6.git
+sudo git clone https://github.com/Dilshanx/jenkins.git
 cd Day6/Code
 sudo docker build -t react-microk8s -f golddockerfile .
 sudo docker tag react-microk8s:latest dilshanjayatissa/react-microk8s:latest ##make sure you did docker login
